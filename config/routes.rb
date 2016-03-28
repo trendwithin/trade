354e275daf49985ffbbd7439ac1989835ae6dd54
Michael Becco <e4e6d4d5@gmail.com>
2016-03-28 16:45:32 -0700

    Add TradeLog Resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :trade_logs
   resources :blogs do
     resources :comments
   end
---
+++ db/migrate/20160328234133_create_trade_logs.rb
@@ -0,0 +1,24 @@
+class CreateTradeLogs < ActiveRecord::Migration
+  def change
+    create_table :trade_logs do |t|
+      t.references :user, index: true, foreign_key: true
+      t.datetime :trade_opened_at, null: false
+      t.string :symbol, null: false
+      t.integer :position_size, null: false
+      t.float :entry_price, null: false
+      t.float :stop
+      t.float :target
+      t.datetime :exit_one_on
+      t.integer :exit_one_shares
+      t.float :exit_one_price
+      t.datetime :exit_two_on
+      t.integer :exit_two_shares
+      t.float :exit_two_price
+      t.datetime :exit_three_on
+      t.integer :exit_three_shares
+      t.float :exit_three_price
+
+      t.timestamps null: false
+    end
+  end
+end
