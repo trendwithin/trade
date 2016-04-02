a5985039bb5e9ceffe91d80a096463b74ba7ca0a
Michael Becco <e4e6d4d5@gmail.com>
2016-04-01 17:00:04 -0700

    Config SendGrid

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,9 +1,23 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "localhost.com",
-  :user_name            => ENV['GMAIL_USERNAME'],
-  :password             => ENV['GMAIL_PASSWORD'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+if Rails.env.development?
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "localhost.com",
+    :user_name            => ENV['GMAIL_USERNAME'],
+    :password             => ENV['GMAIL_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+end
+
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.sendgrid.net",
+    :port                 => 587,
+    :domain               => "heroku.com",
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+end
